c67ebf7f631dd7dbe9d29acc879e5619fff9fb04
Tim DeCillis <timdecillis@gmail.com>
2024-04-17 17:41:43 -0700

    foo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
