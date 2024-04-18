ef29a03cf3b5927e53687b38e878e2216adc52dd
Tim DeCillis <timdecillis@gmail.com>
2024-04-17 17:33:14 -0700

    added About link

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>Hello, Repertoire!</div>
+        <Link to="/about">About</Link>
       </header>
     </div>
   );
