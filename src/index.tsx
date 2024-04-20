35d300714c033d2788ada490a93eb8ceca44ec33
Tim DeCillis <timdecillis@gmail.com>
2024-04-19 19:29:14 -0700

    imported contact

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import {
   Link,
 } from "react-router-dom";
 import Root from './routes/root';
+import Contact from './routes/contact';
 import ErrorPage from './error-page';
 
 const router = createBrowserRouter([
