d3dfbc3fd0ee720e7c81ee3dc938e1963844231b
Tim DeCillis <timdecillis@gmail.com>
2024-04-20 20:32:30 -0700

    imported rootAction

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root, { loader as rootLoader } from "./routes/root";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 import Contact from "./routes/contact";
 import ErrorPage from "./error-page";
 
