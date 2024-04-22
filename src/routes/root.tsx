3ef18bb897568791714c9fb3c5c0b1e09867bfb2
Tim DeCillis <timdecillis@gmail.com>
2024-04-21 21:40:55 -0700

    updated content

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -16,7 +16,7 @@ export default function Root() {
   return (
     <>
       <div id="sidebar">
-        <h1>React Router Contacts</h1>
+        <h1>Your Contacts</h1>
         <div>
           <form id="search-form" role="search">
             <input
