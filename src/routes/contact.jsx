8358822eb8a367de51dbd80c14c8f4db7803fbc9
Tim DeCillis <timdecillis@gmail.com>
2024-04-21 21:36:24 -0700

    added contact

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -7,14 +7,7 @@ export async function loader({ params }) {
 }
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact } = useLoaderData();
 
   return (
     <div id="contact">
