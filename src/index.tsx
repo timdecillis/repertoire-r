f6fd90511a22d400e1e2b6e595de344098b28432
Tim DeCillis <timdecillis@gmail.com>
2024-04-21 21:37:26 -0700

    added loader to contact route

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import Root, {
   loader as rootLoader,
   action as rootAction,
 } from "./routes/root";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader } from "./routes/contact";
 import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
@@ -19,6 +19,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
