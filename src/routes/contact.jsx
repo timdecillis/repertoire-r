d6343e8b1b7c22e4c4068add9e955bd60245cb47
Tim DeCillis <timdecillis@gmail.com>
2024-04-21 21:35:43 -0700

    using loader in contact

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,6 +1,11 @@
 import { Form, useLoaderData } from "react-router-dom";
 import { getContact } from "../contacts";
 
+export async function loader({ params }) {
+  const contact = await getContact(params.contactId);
+  return { contact };
+}
+
 export default function Contact() {
   const contact = {
     first: "Your",
