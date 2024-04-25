dc257b35fb9c0a79993554f7b5b48fe7c711ba94
Tim DeCillis <timdecillis@gmail.com>
2024-04-25 12:34:22 -0700

    updated root loader

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -14,8 +14,10 @@ export async function action() {
   return redirect(`/contacts/${contact.id}/edit`);
 }
 
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({request}: any) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return { contacts };
 }
 
