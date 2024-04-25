b075bd9165fa111b97135a60895965b31e937d31
Tim DeCillis <timdecillis@gmail.com>
2024-04-25 12:36:05 -0700

    formatting

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -14,11 +14,11 @@ export async function action() {
   return redirect(`/contacts/${contact.id}/edit`);
 }
 
-export async function loader({request}: any) {
+export async function loader({ request }: any) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 }
 
 export default function Root() {
