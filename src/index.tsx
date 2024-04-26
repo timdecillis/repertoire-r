576520909ce00783a0cfcfafb85aa17b658af127
Tim DeCillis <timdecillis@gmail.com>
2024-04-26 16:24:25 -0700

    added action to contact route

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,10 @@ import Root, {
   loader as rootLoader,
   action as rootAction,
 } from "./routes/root";
-import Contact, { loader as contactLoader } from "./routes/contact";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
 import ErrorPage from "./error-page";
 import EditContact, { action as editAction } from "./routes/edit";
 import "./index.css";
@@ -25,6 +28,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction,
       },
       {
         path: "contacts/:contactId/edit",
