3889a2b665287122eec583ab3e70cd863a787785
timdecillis <timdecillis@gmail.com>
2024-04-30 16:19:13 -0700

    updated index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,9 @@
 export default function Index() {
   return (
     <p id="zero-state">
-      This is a demo for React Router.
+      You do not have any songs yet
       <br />
-      Check out{" "}
-      <a href="https://reactrouter.com">
-        the docs at reactrouter.com
-      </a>
+
       .
     </p>
   );
