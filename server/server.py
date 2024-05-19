4bd65143b2a50a730d22770be7e8e97c99732c68
timdecillis <timdecillis@gmail.com>
2024-05-19 16:52:08 -0700

    finished basic server

--- server/server.py
+++ server/server.py
@@ -1,9 +1,9 @@
-from Flask import Flask
-app = Flask(__rep_server__)
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return 'hello, world'
+  return 'foobar'
 
-if __rep_server__ == '__main__':
+if __name__ == '__main__':
   app.run(port=5000)
